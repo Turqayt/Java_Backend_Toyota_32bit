65d5556932c6eeb9abf76707c03c40b47fd77de7
Turqay <58841404+Turqayt@users.noreply.github.com>
2022-07-01 13:17:26 +0300

    Delete src directory

--- src/main/java/com/j32bit/backend/dto/form/FormCreateDTO.java
+++
@@ -1,8 +0,0 @@
-package com.j32bit.backend.dto.form;
-
-import lombok.Data;
-
-@Data
-public class FormCreateDTO {
-    private String title;
-}
--- src/main/java/com/j32bit/backend/dto/user/UserRoleAddedDTO.java
+++
@@ -1,9 +0,0 @@
-package com.j32bit.backend.dto.user;
-
-import lombok.Data;
-
-@Data
-public class UserRoleAddedDTO {
-    private Integer role_id;
-    private Integer user_id;
-}
